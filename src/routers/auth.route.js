663e6474ec45f10b79847a4f38a04c32b1b1ed5c
RenanHumberto <renanhumberto61@gmail.com>
2025-04-24 19:38:29 -0300

    muda

--- src/routers/auth.route.js
+++ src/routers/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import authController from '../controllers/auth.controller.js';
+import authMiddleware from '../middleware/auth.middleware.js';
 
 const route = express.Router();
 
 route.post('/login', authController.login);
-route.get('/usuario', authController.getUser); // Corrigido de getUsuario para getUser
+route.get('/usuario', authMiddleware, authController.getUser);
 
 export default route;
