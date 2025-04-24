3a02afa2bd1133b09aa12e9991513ed9f63b6fc0
RenanHumberto <renanhumberto61@gmail.com>
2025-04-24 18:45:46 -0300

    Update CORS to allow Netlify domain

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ import authRoute from './routers/auth.route.js';
 
 dotenv.config(); //carrega as variaveis de ambiente do arquivo .env
 
-const port = process.env.PORT || 3000;
+
 const app = express();
 //pra colocar o meu frontend
 app.use(cors({
-    origin: 'https://santacruzsocios.netlify.app/', // Substitua pelo domínio do seu frontend no Netlify
-    methods: ['GET', 'POST', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
-
+  origin: 'https://santacruzsocios.netlify.app', // Domínio do frontend no Netlify
+  methods: ['GET', 'POST', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 connectDatabase();
 app.use(express.json()); //adiciona suporte para JSON
 app.use("/cadastro", cadastroRoute); //testando git pull 
 app.use("/auth", authRoute);
 
+const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`✅ Servidor rodando na porta ${port}`));
